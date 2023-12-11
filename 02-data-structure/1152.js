a0904e667dbcde53e8241bc711ab848b0b4ee796
허성진 <tjdwls75666@naver.com>
2023-12-11 20:11:35 +0900

    알고리즘 학습

---
+++ 02-data-structure/1152.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+solution(input);
+
+function solution(input) {
+  // Write your code
+  let [char] = input;
+
+  let data = char.trim().split(' ');
+
+  if (data == '') {
+    console.log(0);
+  } else {
+    console.log(data.length);
+  }
+}
