458b89b75656f6a8e30480d6263e834804aeeceb
허성진 <tjdwls75666@naver.com>
2024-02-12 17:27:48 +0900

    백트레킹 알고리즘

---
+++ 06-backtracking/15652.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+solution(input);
+
+function solution(input) {
+  let [n, m] = input[0].split(' ').map(Number);
+  let arr = []; // 순열을 계산하고자 하는 원소가 담긴 배열
+  for (let i = 1; i <= n; i++) arr.push(i);
+  // let visited = new Array(n).fill(false); // 각 원소 인덱스 별 방문 여부
+  let selected = []; // 현재 순열에 포함된 원소
+
+  let answer = '';
+
+  function dfs(arr, depth, start) {
+    if (depth === m) {
+      for (let i = 0; i < selected.length; i++) answer += selected[i] + ' ';
+      answer += '\n';
+      return;
+    }
+
+    for (let i = start; i < arr.length; i++) {
+      selected.push(i + 1);
+      dfs(arr, depth + 1, i);
+      selected.pop();
+    }
+  }
+  dfs(arr, 0, 0);
+
+  console.log(answer);
+}
